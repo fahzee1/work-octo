e0ebc980f046fe7763d7edbcd61646c1ea0e6528
Rylan Francis <rylanfrancis@gmail.com>
2010-12-03 16:56:17 -0600

    banner test

--- etc/django/rylan.py
+++ etc/django/rylan.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/Users/rylanfrancis/Documents/protectamerica2/protectamerica/database.db'
+DATABASE_NAME = '/Users/rylanfrancis/Documents/protectamerica2/protectamerica.dev/database.db'
 DATABASE_USER = ''
 DATABASE_PASSWORD = ''
 
