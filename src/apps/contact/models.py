3764a46972db12f1175dd0d3c82f192d7ee3def6
Robert Johnson <robrocker7@gmail.com>
2011-11-23 10:51:59 -0600

    got the form working and moved it to order

--- src/apps/contact/models.py
+++ src/apps/contact/models.py
@@ -12,12 +12,27 @@ class Submission(models.Model):
         ('sales', 'Sales'),
         ('careers', 'Carriers'),
     )
+
+    HOMEOWNER_CHOICES = (
+        ('YES', 'Yes'),
+        ('NO', 'No'),
+    )
+
+    CREDITRATING_CHOICES = (
+        ('EXCELLENT', 'Excellent (720 or above)'),
+        ('GOOD', 'Good (660 - 719)'),
+        ('AVERAGE', 'Average (620 - 659)'),
+        ('BELOW_AVERAGE', 'Below Average (580 - 619)'),
+        ('POOR', 'Poor (579 or Below)'),
+    )
     
     name = models.CharField(max_length=128)
     email = models.EmailField(max_length=128)
     phone = PhoneNumberField()
     city = models.CharField(max_length=32, blank=True, null=True)
     state = USStateField(blank=True, null=True)
+    address = models.CharField(max_length=128)
+    zipcode = models.CharField(max_length=12)
     package = models.CharField(max_length=24, blank=True, null=True)
     feedback_type = models.CharField(max_length=32, blank=True, null=True)
 
@@ -27,6 +42,14 @@ class Submission(models.Model):
 
     rep_name = models.CharField(max_length=128, blank=True, null=True)
     message = models.TextField(blank=True, null=True)
+
+    homeowner = models.CharField(max_length=3, blank=True, null=True,
+        choices=HOMEOWNER_CHOICES)
+    creditrating = models.CharField(max_length=15, blank=True, null=True,
+        choices=CREDITRATING_CHOICES)
+
+    consent = models.BooleanField(default=False, help_text="By checking this box, I expressly give consent to be contacted according to the Terms and Conditions")
+
     date_created = models.DateTimeField(auto_now_add=True)
 
     
