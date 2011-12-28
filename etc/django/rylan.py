8e4045a90ef82ae646a4044ef0d876cb43484d14
Rylan Francis <rylanfrancis@gmail.com>
2011-12-28 10:34:31 -0600

    added in the home image for copper package

--- etc/django/rylan.py
+++ etc/django/rylan.py
@@ -7,8 +7,7 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/rylanfrancis/Sites/protectamerica/database.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'NAME': '/Users/rylanfrancis/Documents/protectamerica2/protectamerica.dev/database.db',                      # Or path to database file if using sqlite3
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
