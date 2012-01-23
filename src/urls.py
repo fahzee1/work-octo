46e79ce98857ac2acb325b533c57efbb69c02f56
Rylan Francis <rylanfrancis@gmail.com>
2012-01-23 13:52:44 -0600

    added base for crime stats and removed branded product pages

--- src/urls.py
+++ src/urls.py
@@ -95,12 +95,6 @@ else:
         # Home Security Packages
         dtt(r'^home-security-packages/?$', 'packages/index.html', 'security-packages'),
 
-            # Product > Packages
-            dtt(r'^home-security-packages/copper/?$', 'packages/copper.html', 'copper', 'security-packages', ctx={'package_test': 'a'}),
-            dtt(r'^home-security-packages/bronze/?$', 'packages/bronze.html', 'bronze', 'security-packages', ctx={'package_test': 'a'}),
-            dtt(r'^home-security-packages/silver/?$', 'packages/silver.html', 'silver', 'security-packages', ctx={'package_test': 'a'}),
-            dtt(r'^home-security-packages/gold/?$', 'packages/gold.html', 'gold', 'security-packages', ctx={'package_test': 'a'}),
-            dtt(r'^home-security-packages/platinum/?$', 'packages/platinum.html', 'platinum', 'security-packages', ctx={'package_test': 'a'}),
             # Product > Packages B
             dtt(r'^home-security-systems/copper/?$', 'packages/copper-b.html', 'copper-b', 'security-packages', ctx={'package_test': 'b'}),
             dtt(r'^home-security-systems/bronze/?$', 'packages/bronze-b.html', 'bronze-b', 'security-packages', ctx={'package_test': 'b'}),
