7d767902d6799f7bc05c7bbc016b9950502bb6a2
Robert Johnson <robrocker7@gmail.com>
2012-07-05 14:53:36 -0500

    fixed issue with news

--- src/apps/news/urls.py
+++ src/apps/news/urls.py
@@ -9,8 +9,7 @@ news_sitemap = {
 }
 urlpatterns = patterns('apps.news.views',
 
-    url(r'^$', 'news_home', dict(name='news-home',
-        extra_context={'page_name': 'news-home', 'parent': 'support'}),
+    url(r'^$', 'news_home', name='news-home'),
 
     url(r'^archive/$', 'articles', name='news-articles'),
     url(r'^archive/(?P<year>[0-9]{4})/$',
