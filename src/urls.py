2a7b8b391a54bed15a9bf3b778df21cf47c144fb
Robert Johnson <robrocker7@gmail.com>
2012-11-19 15:59:50 -0600

    added package code bones

--- src/urls.py
+++ src/urls.py
@@ -500,6 +500,8 @@ else:
                 #dtt(r'^support/moving-kit/?$', 'support/moving-kit.html', 'moving-kit', 'support'),
                 url(r'^pa/request-moving-kit/security-moving-kit/?$',
                     'apps.contact.views.moving_kit', name='moving-kit'),
+                url(r'^package-code/$',
+                    'apps.pricetable.views.package_code', name='package-code'),
         
         # Affiliate Resources
         
