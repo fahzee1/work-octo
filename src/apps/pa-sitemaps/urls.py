1faf0c0db22fd79b64066e39ad9f24572d556370
mattmhouston <matthew.m.houston@gmail.com>
2013-04-01 11:23:01 -0500

    Update urls.py

--- src/apps/pa-sitemaps/urls.py
+++ src/apps/pa-sitemaps/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^$', 'apps.sitemaps.views.index', name='sitemap'),
+    url(r'^sitemap_test/?$', 'apps.sitemaps.sitemap_dyn.index', name='sitemap'),
 )
 urlpatterns += patterns('django.views.generic.simple',
     url(r'^index.xml$', 'direct_to_template', {
