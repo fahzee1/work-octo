75924c2111fa5bf00ea78d42c6857b5570b87373
CJ Ogbuehi <fahzee1@gmail.com>
2013-06-27 16:50:37 -0500

    10 items total in newsfeed

--- src/apps/newsfeed/middleware.py
+++ src/apps/newsfeed/middleware.py
@@ -30,16 +30,16 @@ class RenderNewsFeed():
 	           
 				try:
 					query1=Q(active=True,city__icontains=city,state__exact=state)
-					feed=TheFeed.objects.select_related().filter(query1)
+					feed=TheFeed.objects.select_related().filter(query1)[:5]
 					ct=feed.count()
 					if ct == 0:
 						request.session['backup']=True
 						feed=FallBacks.objects.all()
 				except TheFeed.DoesNotExist:
-					feed=FallBacks.objects.all()
+					feed=FallBacks.objects.all()[:5]
 
 				fallbacks=FallBacks.objects.select_related().all()
-				request.session['FallBacks']=fallbacks						
+				request.session['FallBacks']=fallbacks[:5]						
 						
 
 	  			if request.session.get('GeoFeedData',False):
