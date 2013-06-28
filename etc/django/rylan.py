c15a72f37a705fb332fe434fe07cfc06b6faae0b
Rylan Francis <rylanfrancis@gmail.com>
2013-06-28 16:52:38 -0500

    updated crime stats and packages

--- etc/django/rylan.py
+++ etc/django/rylan.py
@@ -8,7 +8,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         #'NAME': '/Users/rylanfrancis/Documents/protectamerica2/protectamerica/database.db',                      # Or path to database file if using sqlite3
-	'NAME': '/vagrant/database.db',
+	'NAME': '/vagrant/database2.db',
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
