b52b9b9e233c0546cda7e718fc527496e9970121
Rylan Francis <rylanfrancis@gmail.com>
2013-07-29 09:59:12 -0500

    added new design for paid, added super cookie for skyrocket agent id break out

--- etc/django/common.py
+++ etc/django/common.py
@@ -158,7 +158,7 @@ LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
 
 DEFAULT_PHONE = '8009515190'
-SUPER_AFFILIATES = ("a10058",)
+SUPER_AFFILIATES = ("a10058","a10447","a10449")
 
 
 GEOIP_PATH = os.path.join(settings.PROJECT_ROOT, 'src', 'apps', 'crimedatamodels', 'external', 'data')
