6175c3630e451adc9dba2331a4cab0fbd4807b49
CJ Ogbuehi <fahzee1@gmail.com>
2013-08-12 12:31:35 -0500

    capitalized feed name on presave

--- src/apps/newsfeed/models.py
+++ src/apps/newsfeed/models.py
@@ -13,42 +13,30 @@ from django.core.exceptions import ValidationError
 
 			
 class AddType(models.Model):
-	name=models.CharField(max_length=200,blank=True,null=True,help_text='Type of newsfeed? Two words only for simplicity')
+	name = models.CharField(max_length=200,blank=True,null=True,help_text='Type of newsfeed? Two words only for simplicity')
 
 	def __unicode__(self):
 		return "%s" % (self.name)
 
 	def save(self,*args,**kwargs):
-		try:
-			first,last=self.name.split()
-			_f=first.capitalize()
-			_l=last.capitalize()
-			name=_f+' '+_l
-			self.name=name
-		except ValueError:
-			_name=self.name.split()
-			if len(_name) ==1:
-				_name=self.name.capitalize()
-				self.name=_name
-			else:
-				raise ValidationError('Name should be two words only')
-
+		capital = self.name.title()
+		self.name = capital
 		super(AddType,self).save(*args,**kwargs)
 
 
 
 class TheFeed(models.Model):
-	name=models.CharField(max_length=200,blank=True,null=True,help_text='Name of this feed message')
-	city=models.CharField(max_length=255,blank=True,null=True)
-	state=models.CharField(max_length=2,blank=True,null=True,choices=US_STATES)
-	visible_to_all=models.BooleanField(default=False,help_text='This feed message will be seen by everyone not just a specific location. No need to enter city/state.')
-	message=models.TextField(blank=False,null=False,help_text='Message to show on feed')
-	link=models.URLField(max_length=200,blank=True,null=True,help_text='Link to relevant page')
-	icon=models.CharField(max_length=200,blank=True,null=True,help_text='Icon/Image to show with this feed')
-	expires=models.DateField(blank=False,null=False,help_text='When does this feed expire?')
-	type=models.ForeignKey(AddType,help_text='What type of feed is this?')
-	created=models.DateTimeField(default=timezone.now(),auto_now_add=True)
-	active=models.BooleanField(default=True)
+	name = models.CharField(max_length=200,blank=True,null=True,help_text='Name of this feed message')
+	city = models.CharField(max_length=255,blank=True,null=True)
+	state = models.CharField(max_length=2,blank=True,null=True,choices=US_STATES)
+	visible_to_all = models.BooleanField(default=False,help_text='This feed message will be seen by everyone not just a specific location. No need to enter city/state.')
+	message = models.TextField(blank=False,null=False,help_text='Message to show on feed')
+	link = models.URLField(max_length=200,blank=True,null=True,help_text='Link to relevant page')
+	icon = models.CharField(max_length=200,blank=True,null=True,help_text='Icon/Image to show with this feed')
+	expires = models.DateField(blank=False,null=False,help_text='When does this feed expire?')
+	type = models.ForeignKey(AddType,help_text='What type of feed is this?')
+	created = models.DateTimeField(default=timezone.now(),auto_now_add=True)
+	active = models.BooleanField(default=True)
 
 	def __unicode__(self):
 		return "%s-(type:%s,created:%s)" % (self.name,self.type,self.created)
@@ -59,15 +47,8 @@ class TheFeed(models.Model):
 			if not self.visible_to_all:
 				raise ValidationError('city and state or visible to all required')
 
-		try:
-			f,l=self.city.split()
-			a=f.capitalize()
-			b=l.capitalize()
-			comp=a+' '+b
-			self.city=comp
-		except:
-			a=self.city.capitalize()
-			self.city=a	
+		capital = self.city.title()
+		self.city = capital
 		super(TheFeed,self).save(*args,**kwargs)
 
 
