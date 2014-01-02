43dc991c9925b69945240220146706bc1251c944
Rylan Francis <rylanfrancis@gmail.com>
2014-01-02 13:03:57 -0600

    added time warner

--- src/urls.py
+++ src/urls.py
@@ -176,6 +176,8 @@ elif settings.SITE_ID == 3:
         dtt(r'^vivint/?$', 'affiliates/sem-landing-page/vivint.html', 'vivint'),
         dtt(r'^sloan-security/?$', 'affiliates/sem-landing-page/sloan-security.html', 'sloan-security'),
         dtt(r'^guardian-security/?$', 'affiliates/sem-landing-page/guardian.html', 'guardian'),
+        dtt(r'^time-warner-intelligent-home/?$', 'affiliates/sem-landing-page/time-warner.html', 'time-warner'),
+
 
 
         # TESTS
