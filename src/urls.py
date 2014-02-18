3ba153e86d03a76542eb0efe94f7dc2cdefefa2f
Waldy Przybyslawski <waldemarprzybyslawsk@Marketing03.local>
2014-02-18 16:57:04 -0600

    new shop page for a-b testing

--- src/urls.py
+++ src/urls.py
@@ -496,6 +496,7 @@ else:
 
         # Shop
         dtt(r'^shop-home-security-packages/?$', 'packages/index.html', 'shop'),
+        dtt(r'^shop-home-security-packages-b/?$', 'packages/index-b.html', 'shop-b'),
         dtt(r'^shop-home-security-packages-new/?$', 'packages/index-test.html', 'shop-new'),
 
             # Product > Packages
