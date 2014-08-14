4cbacbdf57be702a308ca7698843081af6f0e37e
CJ Ogbuehi <fahzee1@gmail.com>
2014-08-14 10:52:59 -0500

    adding zhunrize site

---
+++ src/sitesettings/zhunrize.py
@@ -0,0 +1,9 @@
+SITE_ID = 25
+
+from settings import *
+
+CACHES['default']['KEY_PREFIX'] = 'SITE_ID_25__'
+
+DEFAULT_AGENT = 'a10713'
+DEFAULT_SOURCE = 'Organic'
+SESSION_COOKIE_DOMAIN = '.zhunrize.com'
