7fad2468660a7e9fa9c902dff29cae943db82d78
CJ Ogbuehi <fahzee1@gmail.com>
2014-09-17 16:01:55 -0500

    fixed issues with only showing ab test for homesite and running django code for admin

--- src/apps/common/templatetags/ab_test.py
+++ src/apps/common/templatetags/ab_test.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils.safestring import SafeString
 from apps.common.models import AbTest, AbTestCode
+from django.template import Context, Template
 from random import choice
 
 
@@ -9,44 +10,50 @@ register = template.Library()
 @register.inclusion_tag('tests/ab.html')
 def start_ab(request,test=None):
     code = None
-    try:
-        abtest = request.session.get('abtest',None)
-        test = AbTest.objects.get(name=test)
-        if not abtest:
-            if test.code_choices:
-                all = test.code_choices.all()
-                code = choice(all)
-                agent_id = code.aff_id
-                name = code.name
-                code = code.code
-
-                request.session['abtest'] = True
-                request.session['abtest_name'] = [name]
-                request.session['refer_id'] = agent_id
-
-            else:
-                code = '<b>A/B test error. Test name %s doesnt have code choices set, go create AbTestCode in the admin' % test
-
-        else:
-            agent_id = request.session.get('refer_id',None)
-            if test.code_choices and agent_id:
-                try:
-                    code = test.code_choices.get(aff_id=agent_id)
+    agent_id = request.session.get('refer_id',None)
+    if agent_id == 'HOMESITE':
+        try:
+            abtest = request.session.get('abtest',None)
+            test = AbTest.objects.get(name=test)
+            if not abtest:
+                if test.code_choices:
+                    all = test.code_choices.all()
+                    code = choice(all)
+                    agent_id = code.aff_id
+                    name = code.name
                     code = code.code
 
-                except AbTestCode.DoesNotExist:
-                    code = '<b>A/B test error. Test name %s doesnt have code choices with agent_id %s' % (test,agent_id)
+                    request.session['abtest'] = True
+                    request.session['abtest_name'] = [name]
+                    request.session['refer_id'] = agent_id
+
+                else:
+                    code = '<b>A/B test error. Test name %s doesnt have code choices set, go create AbTestCode in the admin' % test
 
             else:
-                code = '<b>A/B test error. Test name %s doesnt have code choices set or agent id isnt set' % test
+                if test.code_choices and agent_id:
+                    try:
+                        code = test.code_choices.get(aff_id=agent_id)
+                        code = code.code
+
+                    except AbTestCode.DoesNotExist:
+                        code = '<b>A/B test error. Test name %s doesnt have code choices with agent_id %s' % (test,agent_id)
+
+                else:
+                    code = '<b>A/B test error. Test name %s doesnt have code choices set or agent id isnt set' % test
 
 
-    except AbTest.DoesNotExist:
-        code = '<b>A/B test error. Test name %s doesnt exist, go create it in the admin' % (test if test else "'N/A'")
+        except AbTest.DoesNotExist:
+            code = '<b>A/B test error. Test name %s doesnt exist, go create it in the admin' % (test if test else "'N/A'")
 
+        t = Template(code)
+        c = Context({})
+        code = t.render(c)
 
-    ctx = {'code':SafeString(code),
+        ctx = {'code':code,
            'name':test}
-    return ctx
+        return ctx
+    else:
+        return None
 
 
