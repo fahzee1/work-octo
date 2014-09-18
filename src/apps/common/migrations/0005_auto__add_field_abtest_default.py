08eba6f0c275671e8d04e915110b8947de6b6e81
CJ Ogbuehi <fahzee1@gmail.com>
2014-09-18 17:01:14 -0500

    small attempt at 50/50 distribution of ab tests and some added template logic

---
+++ src/apps/common/migrations/0005_auto__add_field_abtest_default.py
@@ -0,0 +1,59 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'AbTest.default'
+        db.add_column('common_abtest', 'default', self.gf('django.db.models.fields.related.ForeignKey')(default='', related_name='test', to=orm['common.AbTestCode']), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'AbTest.default'
+        db.delete_column('common_abtest', 'default_id')
+
+
+    models = {
+        'common.abtest': {
+            'Meta': {'object_name': 'AbTest'},
+            'code_choices': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'abtest'", 'symmetrical': 'False', 'to': "orm['common.AbTestCode']"}),
+            'default': ('django.db.models.fields.related.ForeignKey', [], {'default': "''", 'related_name': "'test'", 'to': "orm['common.AbTestCode']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'common.abtestcode': {
+            'Meta': {'object_name': 'AbTestCode'},
+            'aff_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'code': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'common.blackfriday': {
+            'Meta': {'object_name': 'BlackFriday'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'common.linxcontext': {
+            'Meta': {'object_name': 'LinxContext'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'rin': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        },
+        'common.spuncontent': {
+            'Meta': {'object_name': 'SpunContent'},
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '24'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['common']
--- src/apps/common/templatetags/ab_test.py
+++ src/apps/common/templatetags/ab_test.py
@@ -2,44 +2,53 @@ from django import template
 from django.utils.safestring import SafeString
 from apps.common.models import AbTest, AbTestCode
 from django.template import Template, RequestContext
+from django.core.cache import cache
 from random import choice
-
 
 register = template.Library()
 
 @register.inclusion_tag('tests/ab.html')
 def start_ab(request,test=None):
-    #import pdb
-    #pdb.set_trace()
     code = None
     agent_id = request.session.get('refer_id',None)
     if agent_id == 'HOMESITE':
         try:
             abtest = request.session.get(test,None)
-            test = AbTest.objects.get(name=test)
+            abtest_last = cache.get(test)
+            test_obj = AbTest.objects.get(name=test)
             if not abtest:
-                if test.code_choices:
-                    all = test.code_choices.all()
-                    code = choice(all)
-                    aff_id = code.aff_id
-                    name = code.name
-                    code = code.code
+                if test_obj.code_choices:
+                    all = test_obj.code_choices.all()
+                    if not abtest_last:
+                        code = choice(all)
+                        aff_id = code.aff_id
+                        name = code.name
+                        code = code.code
+                    else:
+                        code = all.exclude(name=abtest_last)
+                        if code:
+                            code = code[0]
+                            aff_id = code.aff_id
+                            name = code.name
+                            code = code.code
 
-                    # set the chosen to test code with the test as the key
-                    request.session[test.name] = name
+                    if code and name:
+                        # set the chosen to test code with the test as the key
+                        request.session[test] = name
+                        cache.set(test,name)
 
-                    # if agent_id isnt HOMESITE, set it to aff_id value
-                    if aff_id != agent_id:
-                        request.session['refer_id'] = agent_id
-                        request.session['ab_refer_id'] = agent_id
+                        # if agent_id isnt HOMESITE, set it to aff_id value
+                        if aff_id != agent_id:
+                            request.session['refer_id'] = agent_id
+                            request.session['ab_refer_id'] = agent_id
 
                 else:
                     code = '<b>A/B test error. Test name %s doesnt have code choices set, go create AbTestCode in the admin' % test
 
             else:
-                if test.code_choices:
+                if test_obj.code_choices:
                     try:
-                        code = test.code_choices.get(name=abtest)
+                        code = test_obj.code_choices.get(name=abtest)
                         code = code.code
 
                     except AbTestCode.DoesNotExist:
@@ -54,6 +63,7 @@ def start_ab(request,test=None):
 
 
 
+        # must load tags so django template can render it
         firstline = "{% load content_filters content_tags sekizai_tags testimonial_tags humanize %}\n{% load url from future %}\n"
         code = firstline + code
         t = Template(code)
@@ -63,6 +73,7 @@ def start_ab(request,test=None):
            'name':test}
         return ctx
     else:
+
         return None
 
 
